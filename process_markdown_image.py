419ac9ac94c9d9c02e893b31b7589723647d7d7f
Brian <wl00387719@gmail.com>
2021-07-22 17:35:46 +0800

    fix: Can't find  Obsidian image link properly
    1. Replace using Regex with brute fore method  to search content to avoid  Chinese folder can't  match the pattern
    2. Check if  image link have already have attachment folder, if so, remove it to avoid wrong path get generated.

--- process_markdown_image.py
+++ process_markdown_image.py
@@ -1,35 +1,57 @@
 import re
 import sys
+import os
+from terminal_color import print_header, print_blue
 
 COMMON_IMAGE_REGEX = "\([a-zA-Z0-9%\-\/\.]*\.(png|jpg|jpeg)\)"
-OBSIDIAN_PASTED_IMAGE_REGEX = "Pasted image \d{14}\.(?:png|jpg|jpeg)"
 
-def get_img_name(line):
+def process_obsidian_img_link(line, attachment_folder_name):
+  is_attachment = re.search('!\[\[', line)
+  is_img = re.search("\.(png|jpeg|jpg)\]\]", line)
+  if (is_attachment and is_img):
+    print_header(f'Found Obsidian Image link, Original Markdown:')
+    print_blue(line)
+    # remvoe ![[]]
+    image_name = line.split("[[")[1].split("]]")[0]
+    if image_name.split('/')[0] == attachment_folder_name:
+        # remove attachments folder name
+        return image_name[len(attachment_folder_name)+1:]
+    else:
+        return image_name
+  return None
+    
+
+def get_img_name(line, attachment_folder_name):
   common_img_match = re.search(COMMON_IMAGE_REGEX, line)
   if common_img_match:
+    print_header(f'Found Common Img Link:')
+    print_blue(line)
     # notion export file due to encoding, the white space will become %20
     x = re.sub("%20", " ", common_img_match.group())
     # remove parentheses
-    image_path  =  x[1: len(x) -1]
-    return image_path
+    image_name  =  x[1: len(x) -1]
+    if image_name.split('/')[0] == attachment_folder_name:
+        # remove attachments folder name
+        return image_name[len(attachment_folder_name)+1:]
+    else:
+        return image_name
 
-  obsidian_img_match = re.search(OBSIDIAN_PASTED_IMAGE_REGEX, line)
-  if obsidian_img_match:
-    return obsidian_img_match.group()
-
-  return None
+  image_name = process_obsidian_img_link(line, attachment_folder_name)
+  return image_name if image_name else None
   
   
 def process_markdown_image(content, attachment_path):
   image_path_list = []
   for line_index, line in enumerate(content):
-    image_name = get_img_name(line)
-  
+    attachment_folder_name = os.path.basename(attachment_path)
+    image_name = get_img_name(line, attachment_folder_name)
     if image_name:
+      print_blue(f'image name: {image_name}')
       image_path = {
         "line_index":  line_index,
         "img_path": f"{attachment_path}/{image_name}",
-      } 
+      }
+      print_blue(f'image path: {image_path["img_path"]}') 
       image_path_list.append(image_path)
   
   return image_path_list
